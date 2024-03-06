9588fdbc7751f741dff39a5b33787cbbc6090d8a
Кудрявцев Сергей <sergei.kudryavtsev@gmail.com>
2024-03-06 11:24:02 +0300

    front without errors

--- main.py
+++ main.py
@@ -7,54 +7,59 @@ from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 
 class MainApp(App):
+    path_list = []
     Window.clearcolor = (10, 10, 10, 10)
     main_layout = BoxLayout(orientation="vertical")
 
     def build(self):
+        return self.layout1()
 
-        h_layout = BoxLayout(padding=0, orientation='horizontal')
+
+    def on_press_button1(self, instance):
+        text_input = TextInput(
+            multiline=False, readonly=False, halign="center", font_size=15, size_hint=(1, .03),
+        )
+        self.path_list.append(text_input)
+        self.main_layout.add_widget(text_input)
+
+
+    def on_press_button2(self, instance):
+
+        for i in range(len(self.path_list)):
+            print(self.path_list[i].text)
+
+
+    def on_press_button3(self, instance):
+        self.main_layout.clear_widgets()
+        self.layout1()
+
+
+    def layout1(self):
+        h_layout = BoxLayout(padding=0, orientation='horizontal', size_hint=(1, .2), )
 
         button1 = Button(text='Добавить путь к файлу',
-                        size_hint=(.05, .08),
-                        pos_hint={'center_x': 0.9, 'center_y': 0.95},)
+                         size_hint=(.05, .08),
+                         pos_hint={'center_x': 0.9, 'center_y': 0.95}, )
         button1.bind(on_press=self.on_press_button1)
 
         h_layout.add_widget(button1)
         button2 = Button(text='Сгенерировать таблицы',
-                        size_hint=(.05, .08),
-                        pos_hint={'center_x': 0.6, 'center_y': 0.95})
+                         size_hint=(.05, .08),
+                         pos_hint={'center_x': 0.6, 'center_y': 0.95})
         button2.bind(on_press=self.on_press_button2)
         h_layout.add_widget(button2)
         button3 = Button(text='Очистить экран',
-                        size_hint=(.05, .08),
-                        pos_hint={'center_x': 0.93, 'center_y': 0.95})
+                         size_hint=(.05, .08),
+                         pos_hint={'center_x': 0.93, 'center_y': 0.95})
         button3.bind(on_press=self.on_press_button3)
         h_layout.add_widget(button3)
-        main_layout.add_widget(h_layout)
-
-        main_layout.add_widget(TextInput(
-            multiline=False, readonly=False, halign="center", font_size=25, size_hint=(1, .1),
-        ))
-
-        return main_layout
-
-
-    def on_press_button1(self, instance):
-        main_layout = BoxLayout(orientation="vertical")
-
-        self.col = TextInput(
-            multiline=False, readonly=False, halign="center", font_size=25, size_hint=(1, .1),
+        self.main_layout.add_widget(h_layout)
+        text_input = TextInput(
+            multiline=False, readonly=False, halign="center", font_size=15, size_hint=(1, .03),
         )
-        main_layout.add_widget(self.col)
-
-
-    def on_press_button2(self, instance):
-        print('Вы нажали на кнопку 2!')
-
-
-    def on_press_button3(self, instance):
-        print('Вы нажали на кнопку 3!')
-
+        self.path_list.append(text_input)
+        self.main_layout.add_widget(text_input)
+        return self.main_layout
 
 
 if __name__ == '__main__':
